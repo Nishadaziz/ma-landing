357ea6557dac62961925b74746476de1bae730f1
Nishadaziz <nishad.ctgbd3@gmail.com>
2026-02-09 12:55:18 +0600

    add delploy stamp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,12 @@ import DETServicesSection from "../components/DETServicesSection";
 import BookCTASection from "../components/BookCTASection";
 
 export default function Home() {
+  <div className="fixed bottom-4 left-4 z-50 rounded-lg bg-black px-3 py-2 text-xs font-bold text-white">
+  NEW SITE • commit 57dec3b
+</div>
+
   return (
+    
     <>
       <HeroSection />
       <DETStatsSection />
