57dec3b795010c1c1059892cc88e9a1021372bff
Nishadaziz <nishad.ctgbd3@gmail.com>
2026-02-09 12:50:01 +0600

    Use new site as app entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import AppRouter from "./app/AppRouter";
 
-export default function AppRouter() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-white">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={<div className="p-10 text-3xl font-bold flex justify-center text-slate-300 mt-20">Content goes here</div>}
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+export default function App() {
+  return <AppRouter />;
 }
